5eb814d3baffabd28f653ea5e60777be1253fa6a
Iszuddin Ismail <dino@websegera.my>
2025-06-11 10:48:46 +0800

    password box
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -36,32 +36,42 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            autocomplete="new-password" />
+        <div class="mt-4 block border border-gray-300 rounded-md px-3 py-2 bg-gray-50 text-gray-700 text-sm mb-2">
+            <em >
+                Only enter password to change current password
+            </em>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                autocomplete="new-password" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" 
+                                autocomplete="new-password" />
+
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+
+
+
+            </div>
+
+
         </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" 
-                            autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-
-
-
-        </div>
 
             <x-primary-button class="mt-4">
                 {{ __('Update') }}
